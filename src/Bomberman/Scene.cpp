50510d975dff518c0a7770163924da41e89bdfa5
DavidRecuero <recuero.david@gmail.com>
2017-12-04 19:19:59 +0100

    dimensions window

--- src/Bomberman/Scene.cpp
+++ src/Bomberman/Scene.cpp
@@ -1,9 +1,20 @@
 #include "Scene.h"
+#include <SDL.h>		
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
+#include <iostream>
+#include "Renderer.h"
 
+void Scene::setup()
+{	
 
+	
+}
 
 Scene::Scene()
 {
+	
 }
 
 
--- src/Bomberman/Scene.h
+++ src/Bomberman/Scene.h
@@ -1,8 +1,25 @@
 #pragma once
+#include <SDL.h>		
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
+#include <iostream>
+#include "Renderer.h"
+
 class Scene
-{
+{	
+
+protected: 
+	//size a Constants.h
+
 public:
 	Scene();
+
+	virtual void setup() = 0;
+	virtual void update() = 0;
+	virtual void draw() = 0;
+	virtual void inputHandler() = 0;
+
 	~Scene();
 };
 
